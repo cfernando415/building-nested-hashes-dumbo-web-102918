152d1fd0cc03aa127c009f5f98c1859d9207e14b
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-12 02:02:18 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
       patriarch:, matriarch:, hero:, hero_friends:}, 
     :capulet => {
       #your key/value pairs here!
-      patriarch:, matriarch:, heroine:, heroine_friends:}
+      patriarch:, matriarch:, heroine:, heroine_friends:}}
 
   
 end
